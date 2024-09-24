d9452dcb3454ae64336cce98f18f825a0a9829be
CrisGC <cristiangarcha7@gmail.com>
2024-09-24 11:10:52 +0200

    script1

---
+++ DEMOINI/script.js
@@ -0,0 +1,23 @@
+/* let num1 = 1, num2 = "1";
+console.log("Num1 "+typeof num1);
+console.log("Num2 "+typeof num2);
+console.log(num1 == num1);
+console.log(num1 != num1);
+console.log(num1 !== num1); */
+
+/* let hoy = new Date();
+console.log("Today is "+hoy+"\nI mean "+hoy.getTime()); */
+
+/* var name = "my name";
+var myage = 22;
+if (myage > 18) {
+    name = "another person name";
+}
+console.log(name); */
+
+/* const SALARY = 1000;
+console.log(SALARY);
+//SALARY = 1001; */
+
+let person = {};
+console.log(person);
