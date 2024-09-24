6860e9ada455594e20bacf63c80c1049cdad5e66
CrisGC <cristiangarcha7@gmail.com>
2024-09-24 13:05:59 +0200

    ex1_parte1y2c

--- DEMOINI/Ejercicios_js01.js
+++ DEMOINI/Ejercicios_js01.js
@@ -4,4 +4,7 @@ num1 = prompt("Numero 1");
 num2 = prompt("Numero 2");
 let str = `Sumando ${num1} + ${num2}\nResultado ${+num1+ +num2}`;//+num1+ +num2 es lo mismo que parseInt(num1)+ parseInt(num2)
 console.log(str);
-alert(str)
+alert(str);
+
+/* document.getElementById("parrafo").innerHTML = `${document.getElementById("parrafo").innerHTML}\n${str}`;
+document.body.appendChild(document.createElement(p).innerHTML = "Repito, la respuesta\n"+str); */
