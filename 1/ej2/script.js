dd695f644363d132c1dbaa8b061223bcf314cf05
CrisGC <cristiangarcha7@gmail.com>
2024-10-15 10:26:59 +0200

    Ex_Estaciones_completed

--- 1/ej2/script.js
+++ 1/ej2/script.js
@@ -1,1 +1,5 @@
 //Request four grades for a student, calculate the average, indicate whether the grade is pass, sufficient, good or outstanding (array).
+const grades = [4, 7, 2, 9];
+
+let addition = grades.reduce((a, b) => a + b, 0);
+console.log("La media de las notas es: " + addition/grades.length);
--- x/csv1/script.js
+++ x/csv1/script.js
@@ -1,47 +1,37 @@
+//Csv variable formating
+csv = csvToArray(csv);
+console.log(csv);
+
+//Starting HTML for table
 const lista = document.querySelector(".lista");
-lista.innerHTML=`<thead id="tHead" class="table-dark">
-<tr>
-  <th>a</th>
-  <th>b</th>
+lista.innerHTML = `<thead id="tHead" class="table-dark">
+<tr id="tr0">
 </tr>
 </thead>
 <tbody id="tBody" class="table-group-divider">
-<tr>
-  <td>ja</td>
-  <td>je</td>
-</tr>
-<tr>
-  <td>je</td>
-  <td>ja</td>
-</tr>
-</tbody>`
-/*Elementos de la tabla
-<thead id="tHead">
-<tr>
-  <th>a</th>
-  <th>b</th>
-</tr>
-</thead>
-<tbody id="tBody">
-<tr>
-  <td></td>
-  <td></td>
-</tr>
-<tr>
-  <td></td>
-  <td></td>
-</tr>
-</tbody>`*/
+</tbody>`;
+//Element constants
+const head = document.getElementById("tHead");
+const body = document.getElementById("tBody");
 
+//Header HTML generation
+csv[0].map(e => {
+  document.getElementById("tr0").innerHTML += `<th>
+        ${e}
+        </th>`
+})
+//Body HTML generation
+for(i=1;i<csv.length;i++){
+  body.innerHTML += `<tr id="tr${i}"></tr>`
+  csv[i].map(e => {
+    document.getElementById(`tr${i}`).innerHTML += `<td>
+        ${e}
+        </td>`
+  })
+}
+
+//Function to turn CSV into 2D array
 function csvToArray(csv) {
-    let filas = csv.trim().split("\n");
-    return filas.map(fila => fila.trim().split(";"));
-}
-let resultado;
-if(csv.length>0 &&csv[0].length>0){
-    resultado = csvToArray(csv);
-} else {
-    resultado = "Valor no encontrado"
-}
-
-console.log(resultado);
+  let filas = csv.trim().split("\n");
+  return filas.map(fila => fila.trim().split(";"));
+};
