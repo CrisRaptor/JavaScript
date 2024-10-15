fc309531234d32260646f4914b96a5d25adbd1e7
CrisGC <cristiangarcha7@gmail.com>
2024-10-15 12:51:32 +0200

    Ex_Books_v1

--- x/books/script.js
+++ x/books/script.js
@@ -1,3 +1,31 @@
-//
-console.log(books);
+//console.log(books);
 
+//Starting HTML for table
+const lista = document.querySelector(".lista");
+lista.innerHTML = `<thead id="tHead" class="table-dark">
+<tr id="tr0">
+</tr>
+</thead>
+<tbody id="tBody" class="table-group-divider">
+</tbody>`;
+//Element constants
+const head = document.getElementById("tHead");
+const body = document.getElementById("tBody");
+
+console.log(books[0]);
+//Header HTML generation
+const elements = Object.keys(books[0]);
+elements.forEach(element => {
+    document.getElementById("tr0").innerHTML += `<th>
+    ${element}
+    </th>`
+});
+
+//Body HTML generation
+books.forEach(e => {
+    body.innerHTML += `<tr>`;
+    elements.forEach(element => {
+        body.innerHTML += `<td>${e[element]}</td>`;
+    });
+    body.innerHTML += `</tr>`;
+}); 
