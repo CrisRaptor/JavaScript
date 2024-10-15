6a67ddd90fe33875b7d67cf9fda06cc1ddc5ae47
CrisGC <cristiangarcha7@gmail.com>
2024-10-15 11:02:26 +0200

    Ex_Books_Start

---
+++ x/books/values.js
@@ -0,0 +1,9 @@
+let books = [
+    {Title:"",Genre:"",Author:[""],Pages:0, Published: Date(""), Read:false, Website:""},
+    {Title:"",Genre:"",Author:[""],Pages:0, Published: Date(""), Read:false, Website:""},
+    {Title:"",Genre:"",Author:[""],Pages:0, Published: Date(""), Read:false, Website:""},
+    {Title:"",Genre:"",Author:[""],Pages:0, Published: Date(""), Read:false, Website:""},
+    {Title:"",Genre:"",Author:[""],Pages:0, Published: Date(""), Read:false, Website:""},
+    {Title:"",Genre:"",Author:[""],Pages:0, Published: Date(""), Read:false, Website:""},
+    {Title:"",Genre:"",Author:[""],Pages:0, Published: Date(""), Read:false, Website:""}
+];
